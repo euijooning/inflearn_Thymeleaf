a80f9ac5d826b1997911d1763b060db1be000e23
EUIJOON <euijoonism@gmail.com>
2023-09-28 16:00:00 +0900

    (10) 주석

--- src/main/java/inflearn/thymeleaf/basic/BasicController.java
+++ src/main/java/inflearn/thymeleaf/basic/BasicController.java
@@ -106,6 +106,12 @@ public class BasicController {
         model.addAttribute("users", list);
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
 
 
 
