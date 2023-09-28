c23ad95baeb64672ec68a089fb1095b2b49fbd42
EUIJOON <euijoonism@gmail.com>
2023-09-28 17:57:52 +0900

    (12) 자바스크립트 인라인

--- src/main/java/inflearn/thymeleaf/basic/BasicController.java
+++ src/main/java/inflearn/thymeleaf/basic/BasicController.java
@@ -119,6 +119,14 @@ public class BasicController {
     }
 
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
+
 
 
     @Component("helloBean")
